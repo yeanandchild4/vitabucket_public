a55433f1399f1714eece83011cb98b4bf7cae936
yony-k <109204976+yony-k@users.noreply.github.com>
2024-05-07 17:57:12 +0900

    Create stop.sh

---
+++ scripts/stop.sh
@@ -0,0 +1,5 @@
+cd /home/tomcat/apache-tomcat-10.1.20/bin
+sh ./shutdown.sh
+rm -rf /home/tomcat/apache-tomcat-10.1.20/work/Catalina/localhost/ROOT
+rm -rf /home/tomcat/apache-tomcat-10.1.20/webapps/ROOT
+rm -rf /home/tomcat/apache-tomcat-10.1.20/webapps/ROOT.war
